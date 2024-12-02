9f0ca9e14ca8aa60552125e545f443989fe5cbce
BCD15 <bruno69correadias@gmail.com>
2024-12-02 13:22:47 -0300

    menu hamburg

--- Home/home.js
+++ Home/home.js
@@ -1,3 +1,13 @@
+function menuHamburgOpen() {
+  const menuHamburg = document.querySelectorAll(".mh");
+  const sla = document.querySelector(".sla");
+
+  sla.classList.toggle("dispFlex");
+  menuHamburg.forEach((item) => {
+    item.classList.toggle("dispNone");
+  }); 
+}
+
 function GetCardItem() {
   const cardItem = JSON.parse(localStorage.getItem("cardItens"));
   const cardItem0 = cardItem[0]
